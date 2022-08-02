db37b3e93578016623d258e74539340b00d0bf4d
hagenrahardjo <hagengame2001@gmail.com>
2022-08-02 18:41:23 +0700

    Add function to score program

--- broken_score.py
+++ broken_score.py
@@ -3,14 +3,20 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# TODO: Fix this!
+def main():
+    score = float(input("Enter score: "))
+    print(determine_status(score))
 
-score = float(input("Enter score: "))
-if score > 100 or score < 0:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Pass")
-else:
-    print("Bad")
+
+def determine_status(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
